15e8b494dfa73df3aff0dc851a1559b1a2abb01c
amora7741 <mora7741@gmail.com>
2024-03-08 14:20:46 -0800

    Stack items if user adds same item on seperate occasions

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,22 @@ function Layout() {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item, quantity) => {
-    setCart((prevCart) => [...prevCart, { item: item, quantity: quantity }]);
+    setCart((prevCart) => {
+      const existingItemIndex = prevCart.findIndex(
+        (cartItem) => cartItem.item.id === item.id
+      );
+
+      if (existingItemIndex >= 0) {
+        const updatedCart = [...prevCart];
+        updatedCart[existingItemIndex] = {
+          ...updatedCart[existingItemIndex],
+          quantity: updatedCart[existingItemIndex].quantity + quantity,
+        };
+        return updatedCart;
+      } else {
+        return [...prevCart, { item: item, quantity: quantity }];
+      }
+    });
   };
 
   return (
