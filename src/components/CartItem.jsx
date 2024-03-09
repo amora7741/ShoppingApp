e88a86b2d0b250105166132047c87b12d2cc738d
amora7741 <mora7741@gmail.com>
2024-03-09 15:15:07 -0800

    Adjust font sizes

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,9 +3,11 @@ function CartItem({ imageUrl, name, quantity, price }) {
     <div className='cart-item-card'>
       <img src={imageUrl} alt='' className='itemimg cartitemimg' />
       <div className='cart-card-right'>
-        <p>{name}</p>
-        <p>Qty: {quantity}</p>
-        <p>Total: ${quantity * price}</p>
+        <div>
+          <p>{name}</p>
+          <p>Qty: {quantity}</p>
+          <p>Total: ${quantity * price}</p>
+        </div>
         <button id='remove' className='underline'>
           Remove
         </button>
