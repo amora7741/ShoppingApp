7df8d68e615055a8a90090f373ba26381d7bde64
amora7741 <mora7741@gmail.com>
2024-03-09 14:49:30 -0800

    Style cart items, display quantity and total price of each

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,11 @@
 function CartItem({ imageUrl, name, quantity, price }) {
   return (
     <div className='cart-item-card'>
-      <div className='cart-card-left'>
-        <img src={imageUrl} alt='' className='itemimg cartitemimg' />
+      <img src={imageUrl} alt='' className='itemimg cartitemimg' />
+      <div className='cart-card-right'>
+        <p>{name}</p>
+        <p>Qty: {quantity}</p>
+        <p>Total: ${quantity * price}</p>
       </div>
     </div>
   );
