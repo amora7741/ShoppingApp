335d26feff944d1e1e60edb1dab9cddd5550d500
amora7741 <mora7741@gmail.com>
2024-03-10 14:16:59 -0700

    Resize cart page on smaller screens

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,14 @@
 function CartItem({ imageUrl, name, quantity, price, onClick }) {
   return (
     <div className='cart-item-card'>
-      <img src={imageUrl} alt='' className='itemimg' />
+      <div className='cart-item-image-container'>
+        <img src={imageUrl} alt='' className='cart-item-image' />
+      </div>
       <div className='cart-card-right'>
-        <div>
-          <p>{name}</p>
+        <div className='cart-item-info'>
+          <p className='cart-item-name'>{name}</p>
           <p>Qty: {quantity}</p>
-          <p>Total: ${quantity * price}</p>
+          <p>Total: ${(quantity * price).toFixed(2)}</p>
         </div>
         <button id='remove' className='underline' onClick={onClick}>
           Remove
