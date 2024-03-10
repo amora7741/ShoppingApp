9fda7d94997e979439195f3116b14e118eb2b633
amora7741 <mora7741@gmail.com>
2024-03-09 21:44:50 -0800

    Add hamburger menu for smaller screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,53 @@
-import { Link } from 'react-router-dom';
-
-import BreezeLogo from '../assets/logo.svg';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import BreezeLogo from "../assets/logo.svg";
 
 function Header({ cart, totalQuantity }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const showItems = () => {
     console.log(cart);
   };
 
   return (
     <nav>
-      <div className='logo'>
-        <img src={BreezeLogo} alt='Logo' />
-        <Link to='/'>Breeze</Link>
+      <div className="logo">
+        <img src={BreezeLogo} alt="Logo" />
+        <Link to="/">Breeze</Link>
       </div>
 
-      <div className='redirects'>
-        <Link to='/' className='underline'>
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <div className={`menu ${isMenuOpen ? "open" : ""}`}>
+        <Link to="/" className="underline" onClick={toggleMenu}>
           Home
         </Link>
 
-        <Link to='shop' className='underline'>
+        <Link to="shop" className="underline" onClick={toggleMenu}>
           Shop
         </Link>
-        <Link to='about' className='underline'>
+
+        <Link to="about" className="underline" onClick={toggleMenu}>
           About
         </Link>
+
+        <Link
+          to="cart"
+          className="underline"
+          onClick={() => {
+            showItems();
+            toggleMenu();
+          }}
+        >
+          Cart: {totalQuantity} Items
+        </Link>
       </div>
-      <Link to='cart' className='underline' onClick={showItems}>
-        Cart: {totalQuantity} Items
-      </Link>
     </nav>
   );
 }
