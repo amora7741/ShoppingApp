7fe80f16491a4a1c62fe88ad6cc3151631914da6
amora7741 <mora7741@gmail.com>
2024-03-09 21:55:00 -0800

    Revert "Add hamburger menu for smaller screens"
    This reverts commit 9fda7d94997e979439195f3116b14e118eb2b633.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,34 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import BreezeLogo from "../assets/logo.svg";
+import { Link } from 'react-router-dom';
+
+import BreezeLogo from '../assets/logo.svg';
 
 function Header({ cart, totalQuantity }) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   const showItems = () => {
     console.log(cart);
   };
 
   return (
     <nav>
-      <div className="logo">
-        <img src={BreezeLogo} alt="Logo" />
-        <Link to="/">Breeze</Link>
+      <div className='logo'>
+        <img src={BreezeLogo} alt='Logo' />
+        <Link to='/'>Breeze</Link>
       </div>
 
-      <button className="hamburger" onClick={toggleMenu}>
-        ☰
-      </button>
-
-      <div className={`menu ${isMenuOpen ? "open" : ""}`}>
-        <Link to="/" className="underline" onClick={toggleMenu}>
+      <div className='redirects'>
+        <Link to='/' className='underline'>
           Home
         </Link>
 
-        <Link to="shop" className="underline" onClick={toggleMenu}>
+        <Link to='shop' className='underline'>
           Shop
         </Link>
-
-        <Link to="about" className="underline" onClick={toggleMenu}>
+        <Link to='about' className='underline'>
           About
         </Link>
-
-        <Link
-          to="cart"
-          className="underline"
-          onClick={() => {
-            showItems();
-            toggleMenu();
-          }}
-        >
-          Cart: {totalQuantity} Items
-        </Link>
       </div>
+      <Link to='cart' className='underline' onClick={showItems}>
+        Cart: {totalQuantity} Items
+      </Link>
     </nav>
   );
 }
